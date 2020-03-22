953d46b62fb4e698d1945098f6d292305d26d71a
Thomas Briggs <thomas.briggs333@gmail.com>
2020-03-22 14:02:04 +0000

    Amount if first arg, max is 2nd

--- src/randarray.cpp
+++ src/randarray.cpp
@@ -6,11 +6,11 @@ int main(int argc, char const *argv[])
 {
     using namespace std;
     srand((unsigned)time(0));
-    int amount = stoi(argv[2]);
-    int max = stoi(argv[1]);
+    int amount = stoi(argv[1]);
+    int max = stoi(argv[2]);
     for (int i = 0; i < amount - 1; i++)
     {
-        cout << (rand() % max) << " ,";
+        cout << (rand() % max) << ", ";
     }
     cout << (rand() % max) << endl;
     return 0;
