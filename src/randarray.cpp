d02704f915ef993ed7fa0e476d194ad7a39d3d58
Thomas Briggs <thomas.briggs333@gmail.com>
2020-03-22 14:06:45 +0000

    If only 1 arg are given max is amount * 10 else 2nd arg is maxa

--- src/randarray.cpp
+++ src/randarray.cpp
@@ -7,7 +7,12 @@ int main(int argc, char const *argv[])
     using namespace std;
     srand((unsigned)time(0));
     int amount = stoi(argv[1]);
-    int max = stoi(argv[2]);
+    int max = (amount * 10);
+    if (argc == 3)
+    {
+        max = stoi(argv[2]);
+    }
+
     for (int i = 0; i < amount - 1; i++)
     {
         cout << (rand() % max) << ", ";
